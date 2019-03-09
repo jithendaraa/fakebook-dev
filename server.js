1eebbf8df013d87cbfb4d63f66f0ba31622b1530
jithendaraa <jithendaraa>
2019-03-09 06:47:30 +0530

    server side passportjs script for google oAuth2.0

--- server.js
+++ server.js
@@ -1,27 +1,22 @@
 const express = require('express');
+require('./services/passport');
 const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
 
 let urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const app = express();
+authRoutes(app);
 
-app.get('/api', (req, res) => {
-  const customers = [
-    { id: 1, firstName: 'aa', lastName: 'Doe' },
-    { id: 2, firstName: 'Brad', lastName: 'Traversy' },
-    { id: 3, firstName: 'Mary', lastName: 'Swanson' },
-  ];
+// console.developers.google.com
 
-  res.json(customers);
-});
+// app.post('/signin', urlencodedParser, (req, res) => {
+//   // res.send("Welcome to Post Req" + req.body.name);
+//   console.log(req.body);
+//   // res.render("app", {data: req.body});
+//   // req.render('./client/app', )
+// })
 
-app.post('/signin', urlencodedParser, (req, res) => {
-    // res.send("Welcome to Post Req" + req.body.name);
-    console.log(req.body);
-    // res.render("app", {data: req.body});
-    // req.render('./client/app', )
-})
+const PORT = process.env.PORT || 5000;
 
-const port = 5000;
-
-app.listen(port, () => `Server running on port ${port}`);
+app.listen(PORT, () => `Server running on port ${PORT}`);
