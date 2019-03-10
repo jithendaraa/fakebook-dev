34eb2ced84d779bb99e41476f0b4dd22f03a24e0
jithendaraa <jithendaraa>
2019-03-10 13:30:12 +0530

    dummy route for / added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,11 @@
 const passport = require('passport');
 
 module.exports = (app) => {
+
+    app.get('/', (req, res) => {
+        res.send("hi");
+    });
+
     app.get(
         '/auth/google',
         passport.authenticate("google", {
