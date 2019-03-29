f97b5233040671fdac3794b7c5dc1119f0a544b9
jithendaraa <jithen.subra@gmail.com>
2019-03-29 19:59:34 +0530

    css problem in chatbox fixed

--- client/src/components/loggedInComponents/Chat/Chatbox.js
+++ client/src/components/loggedInComponents/Chat/Chatbox.js
@@ -23,6 +23,21 @@ class Chatbox extends Component {
     parentDivChild = textObj => {
         let displayTexts = document.getElementById("displayTexts");
         let parentDiv = document.createElement("div");
+
+        let dpThisUser = document.createElement("img");
+        let dpThatUser = document.createElement("img");
+
+        dpThatUser.src = "images/" + this.props.openChat.image;
+        dpThisUser.src = "images/" + this.props.auth.image;
+
+        dpThisUser.style.width = "20px";
+        dpThisUser.style.height = "20px";
+        dpThisUser.style.borderRadius = "10px";
+
+        dpThisUser.style.width = "20px";
+        dpThisUser.style.height = "20px";
+        dpThisUser.style.borderRadius = "10px";
+        
         let p = document.createElement("p");
         p.innerHTML = textObj.fromName.split(" ")[0] + ": " + textObj.message;
         p.style.display = "inline-block";
@@ -66,7 +81,10 @@ class Chatbox extends Component {
 
         this.props.socket.on('message', textObj => {
             this.parentDivChild(textObj);
-            this.playSound();
+            if(textObj.fromId != this.props.auth._id){
+                this.playSound();
+            }
+            
         })
 
 
@@ -153,93 +171,4 @@ const mapStateToProps = state => {
 export default connect(mapStateToProps, actions)(Chatbox);
 
 
-
-//On Connection to sockets
-
-        // this.state.socket.on("connect", () => {
-
-        //     this.setState({ socketId: this.state.socket.id });
-        //     let displayTexts = document.getElementById("displayTexts");
-
-        //     //Online users socket events -- start
-        //     this.state.socket.emit("online users", { 
-        //         name: this.props.auth.displayName, 
-        //         userId: this.props.auth._id, 
-        //         socketId: this.state.socket.id 
-        //     });
-
-        //     this.state.socket.on('online users', onlineUsers => {
-        //         console.log(onlineUsers);
-        //         this.setState({ onlineUsers: onlineUsers });
-        //     });
-        //     //Online users socket events -- end
-
-
-        //     //Socket event to get pre-existing chats from DB -- start
-        //     this.state.socket.on('output', async chats => {
-        //         let texts = [];
-        //         chats.map(chat => {
-        //             if (chat.fromId == this.props.auth._id || chat.toId == this.props.auth._id) {
-        //                 texts.push(chat);
-        //             }
-        //         });
-        //         await this.setState({ chats: texts });
-
-        //         if (texts.length > 0) {
-        //             texts.map(text => {
-        //                 let parentDiv = document.createElement("div");
-        //                 let p = document.createElement("p");
-        //                 p.innerHTML = text.fromName.split(" ")[0] + ": " + text.message;
-        //                 p.style.display = "inline-block";
-        //                 p.style.padding = "2px 5px";
-        //                 p.style.borderRadius = "10px";
-        //                 p.style.backgroundColor = "black";
-        //                 p.style.color = "goldenrod";
-        //                 p.style.wordBreak = "break-all";
-        //                 p.style.maxWidth = "200px";
-        //                 if (text.fromId == this.props.auth._id) {
-        //                     p.style.backgroundColor = "goldenrod";
-        //                     p.style.color = "black";
-        //                     parentDiv.align = "right";
-        //                     p.style.textAlign = "left";
-        //                 }
-        //                 parentDiv.appendChild(p);
-        //                 // console.log(displayTexts.clientHeight());
-        //                 // console.log(displayTexts.height());
-        //                 displayTexts.appendChild(parentDiv);
-        //             });
-        //         }
-        //         displayTexts.scrollTop = displayTexts.scrollHeight;
-        //     });
-        //     //Socket event to get pre-existing chats from DB -- end
-
-
-        //     //Socket event to listen for messages -- start
-        //     this.state.socket.on('message', textObj => {
-        //         console.log("received")
-        //         let parentDiv = document.createElement("div");
-        //         let p = document.createElement("p");
-        //         p.innerHTML = textObj.fromName.split(" ")[0] + ": " + textObj.message;
-        //         p.style.display = "inline-block";
-        //         p.style.padding = "2px 5px";
-        //         p.style.borderRadius = "10px";
-        //         p.style.backgroundColor = "black";
-        //         p.style.color = "goldenrod";
-        //         p.style.wordBreak = "break-all";
-        //         p.style.maxWidth = "200px";
-        //         if (textObj.fromId == this.props.auth._id) {
-        //             p.style.backgroundColor = "goldenrod";
-        //             p.style.color = "black";
-        //             parentDiv.align = "right";
-        //             p.style.textAlign = "left";
-        //         }
-        //         parentDiv.appendChild(p);
-        //         displayTexts.appendChild(parentDiv);
-        //         displayTexts.scrollTop = displayTexts.scrollHeight;  
-        //     });
-
-            //Socket event to listen for messages -- end
-        //     console.log("connected to sockets");
-        // });
-
-        //Connection to sockets end          
+       
