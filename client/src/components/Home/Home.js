368ccea8a11636ef505acb7670136bc99275c930
jithendaraa <jithen.subra@gmail.com>
2019-03-31 04:52:16 +0530

    set and corrected bg problems in non-loggedInComponents

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,31 +6,33 @@ import { connect } from 'react-redux';
 import NavigationItems from '../Navigation/NavigationItems';
 import Header from '../loggedInComponents/Header';
 import Dashboard from '../loggedInComponents/Dashboard/Dashboard';
-import Spinner from '../UI/Spinner/Spinner'
+import Spinner from '../UI/Spinner/Spinner';
+import GOAuthBtn from '../UI/Button/GoogleOAuthBtn';
 
 
 
 const guestHome = (
-    <div className={classes.guestHome}>
-        <div className={classes.navbar}>
+    <div className={classes.guestHome} style={{backgroundImage: "linear-gradient(to right, rgb(0,0,0) , rgb(100,100,100))"}}>
+        <div className={classes.navbar} style={{backgroundImage: "linear-gradient(to right, rgb(0,0,0) , rgb(100,100,100))"}}>
             <h2 className={classes.header}>Fakebook</h2>
-            <NavigationItems />
+            {/* <NavigationItems /> */}
         </div>
         <center className={classes.pad}>
             <div className={classes.separator}></div>
             <br />
-            <div className={classes.pageContent}>
+            <div className={classes.pageContent} >
                 <h3>Welcome to Fakebook</h3>
                 <h5>This is the Homepage of the Guest!</h5>
                 <p>
                     This is a project which took its inspiration from Facebook.<br />
-                    This is a fake facebook ripoff using React/Redux/express/Node/passport with MongoDB.<br />
-                    Date of start: 7th March 2019
+                    This is a fake facebook ripoff using React/Redux/express/Node with MongoDB.<br />
+                    {/* Date of start: 7th March 2019 */}
                 </p>
                 <p>You are currently a guest and not logged in!</p>
-                <p>Please signup or Signin to continue.</p>
+                <p>Please Signin</p>
             </div>
         </center>
+        <div style={{paddingTop: "120px"}}><GOAuthBtn /></div>
     </div>
 );
 
--- client/src/components/Navigation/NavigationItems.js
+++ client/src/components/Navigation/NavigationItems.js
@@ -5,9 +5,9 @@ import classes from './NavigationItems.css';
 
 const navigationItems = () => (
     <ul className={classes.Navbar}>
-        <NavigationItem link="/" linkTo="Home"></NavigationItem>
-        <NavigationItem link="/signup" linkTo="Sign Up"></NavigationItem>
-        <NavigationItem link="/signin" linkTo="Sign In"></NavigationItem>
+        {/* <NavigationItem link="/" linkTo="Sign Up"></NavigationItem> */}
+        {/* <NavigationItem link="/signup" linkTo="Sign Up"></NavigationItem> */}
+        {/* <NavigationItem link="/signin" linkTo="Sign In"></NavigationItem> */}
     </ul>
 );
 
